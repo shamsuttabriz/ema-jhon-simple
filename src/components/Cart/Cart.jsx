c99aed20fe35256e27348a3e12bc93ac3747f91f
shamsuttabriz <maria04182017@gmail.com>
2023-11-23 00:16:21 +0600

    Show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,9 +5,15 @@ export const Cart = (props) => {
     const { cart } = props;
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice += product.price;
-        totalShipping += product.shipping;
+        // product.quantity = product.quantity || 1;
+        if (product.quantity === 0) {
+            product.quantity = 1;
+        }
+        totalPrice += product.price * product.quantity;
+        totalShipping += product.shipping * product.quantity;
+        quantity += product.quantity;
     }
 
     const totalTax = totalPrice * 7 / 100;
@@ -16,7 +22,7 @@ export const Cart = (props) => {
     return (
         <div className='cart-container'>
             <h3>Cart Container</h3>
-            <p><b>Selected Item:</b> {cart.length}  </p>
+            <p><b>Selected Item:</b> {quantity}  </p>
             <p><b>Total Price:</b> ${totalPrice} </p>
             <p><b>Total Shopping Price:</b> ${totalShipping} </p>
             <p><b>Tax:</b> ${totalTax.toFixed(2)} </p>
